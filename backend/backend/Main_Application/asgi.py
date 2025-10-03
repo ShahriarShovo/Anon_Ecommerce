4cd239ad286b1d778ab4eec1ae192e73b93270cf
ShahriarShovo <developer.shahriar.rahaman@gmail.com>
2025-10-03 06:38:28 +0600

    again

--- backend/backend/Main_Application/asgi.py
+++ backend/backend/Main_Application/asgi.py
@@ -13,8 +13,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.middleware import BaseMiddleware
 from channels.db import database_sync_to_async
-from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Main_Application.settings')
 
@@ -27,6 +25,9 @@ class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
         from django.contrib.auth.models import AnonymousUser
         from django.contrib.auth import get_user_model
+        from rest_framework_simplejwt.tokens import AccessToken
+        from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+
         User = get_user_model()
         # Get token from query parameters
         query_string = scope.get('query_string', b'').decode()
@@ -58,6 +59,9 @@ class JWTAuthMiddleware(BaseMiddleware):
         return await super().__call__(scope, receive, send)
     
     async def get_user(self, user_id):
+        from django.contrib.auth.models import AnonymousUser
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
         """Get user by ID"""
         try:
             return await database_sync_to_async(User.objects.get)(id=user_id)
